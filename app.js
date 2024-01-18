33ce06e4ea389a4d9c32cb6b8e66e0100b389cf3
Samuel Magno <smagno.db@gmail.com>
2024-01-18 18:19:19 -0300

    alterando limite para 100

--- app.js
+++ app.js
@@ -13,11 +13,11 @@ exibirMensagemInicial();
 
 function exibirMensagemInicial(){
     exibirTextoNaTela('h1', 'Jogo do número secreto!');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 a 100');
 }
 
 exibirTextoNaTela('h1', 'Jogo do número secreto!');
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+exibirTextoNaTela('p', 'Escolha um número entre 1 a 100');
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
